6f9768082ca86c48a11d370ee12a21b07a536867
Zhizhou Qiu <qzhizhou@bu.edu>
2019-04-12 19:16:28 -0400

    Criterion

--- src/course/CriComp.java
+++ src/course/CriComp.java
@@ -1,5 +1,49 @@
 package course;
 
-public class CriComp {
+/**
+ * @description: this a class to manage the weight of every assignment/exam/project
+ * @author: Zhizhou Qiu
+ * @date: 04-12-2019
+ */
+public class CriComp extends Criterion{
+    private final static double DEFAULT_TOTALSCORE = 100.0;
+    private final static int DEFAULT_NUMBER = 2;
+    private double weights;
+    private double toatalScore;
 
+    public CriComp(double weights, double totalScore){
+        this.weights = weights;
+        this.toatalScore = totalScore;
+    }
+
+    /**
+    * @Description: default constructor,
+    * @Author: Zhizhou Qiu
+    * @Date: 2019/4/12
+    **/
+    public CriComp() {
+        this.weights = 1.0 / DEFAULT_NUMBER;
+        this.toatalScore = DEFAULT_TOTALSCORE;
+    }
+
+    /**
+    * @Description: getters and setters for current weight and totalscore
+    * @Author: Zhizhou Qiu
+    * @Date: 2019/4/12
+    **/
+    public double getToatalScore() {
+        return toatalScore;
+    }
+
+    public void setToatalScore(double toatalScore) {
+        this.toatalScore = toatalScore;
+    }
+
+    public double getWeights() {
+        return weights;
+    }
+
+    public void setWeights(double weights) {
+        this.weights = weights;
+    }
 }
--- src/course/Criterion.java
+++ src/course/Criterion.java
@@ -1,5 +1,127 @@
 package course;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @descriptions: this is a class that when a course is initialized,
+ * the number of assignments, exams etc are assigned and each weight is clarified
+ */
 public class Criterion {
 
+    private final static int DEFAULT_NUMBER = 2;
+    private List<CriComp> assignments;
+    private List<CriComp> exams;
+    private List<CriComp> projects;
+    private double weightsOfAssignments;
+    private double weightsOfExams;
+    private double weightsOfProjects;
+    private double weightsOfAttendance;
+    private int numberOfAssignments;
+    private int numberOfExams;
+    private int numberOfProjects;
+
+
+    /**
+     * @Description: the constructors of Criterion, by default the params are valid
+     * @Param: double: weightsOfAssignments,      int: numberOfAssignments
+     *                  weightsOfExams,                 numberOfExams
+     *                  weightsOfProjects,              numberOfProjects
+     *                  weightsOfAttendance,
+     * @Author: Zhizhou Qiu
+     * @Date: 2019/4/12
+     **/
+    public Criterion(
+            double weightsOfAssignments,
+            double weightsOfExams,
+            double weightsOfProjects,
+            double weightsOfAttendance,
+            int numberOfAssignments,
+            int numberOfExams,
+            int numberOfProjects
+    ) {
+        this.weightsOfAssignments = weightsOfAssignments;
+        this.weightsOfAttendance = weightsOfAttendance;
+        this.weightsOfExams = weightsOfExams;
+        this.weightsOfProjects = weightsOfProjects;
+        this.numberOfAssignments = numberOfAssignments;
+        this.numberOfExams = numberOfExams;
+        this.numberOfProjects = numberOfProjects;
+        assignments = new ArrayList<>();
+        exams = new ArrayList<>();
+        projects = new ArrayList<>();
+    }
+
+    /**
+    * @Description: default constructor
+    * @Param:
+    * @Return:
+    * @Author: Zhizhou Qiu
+    * @Date: 2019/4/12
+    **/
+    public Criterion() {
+        createDefaultCriterion();
+    }
+
+    public int getNumberOfAssignments() {
+        return numberOfAssignments;
+    }
+
+    public int getNumberOfExams() {
+        return numberOfExams;
+    }
+
+    public int getNumberOfProjects() {
+        return numberOfProjects;
+    }
+
+    public List<CriComp> getAssignments() {
+        return assignments;
+    }
+
+    public List<CriComp> getExams() {
+        return exams;
+    }
+
+    public List<CriComp> getProjects() {
+        return projects;
+    }
+
+    public double getWeightsOfAssignments() {
+        return weightsOfAssignments;
+    }
+
+    public double getWeightsOfExams() {
+        return weightsOfExams;
+    }
+
+    public double getWeightsOfProjects() {
+        return weightsOfProjects;
+    }
+
+    public double getWeightsOfAttendance() {
+        return weightsOfAttendance;
+    }
+
+    /**
+     * @Description: create default criterion, for each category the default number is 2, and the weight is 0.25
+     * @Param:
+     * @Return:
+     * @Author: Zhizhou Qiu
+     * @Date: 2019/4/12
+     **/
+    private void createDefaultCriterion() {
+        weightsOfProjects = 0.25;
+        weightsOfExams = 0.25;
+        weightsOfAttendance = 0.25;
+        weightsOfAssignments = 0.25;
+        assignments = new ArrayList<>();
+        projects = new ArrayList<>();
+        exams = new ArrayList<>();
+        for (int i = 0; i < DEFAULT_NUMBER; i++) {
+            assignments.add(new CriComp());
+            projects.add(new CriComp());
+            exams.add(new CriComp());
+        }
+    }
 }
