d01d7939d24f66f7b779a6560268055ed64ebe68
mmao95 <zhumaomao509@gmail.com>
2019-04-16 15:58:12 -0400

    Update Course.java

--- src/course/Course.java
+++ src/course/Course.java
@@ -1,9 +1,102 @@
 package course;
 
-public class Course {
+import grade.Grade;
+import personal.Student;
 
+import java.util.HashMap;
+
+public class Course implements Analysis{
+    private String cname;
     private String cid;
-    private String name;
-    private Criterion criterion;
-
+    private String semester;
+    private String cyear;
+    private Criterion ccriterion;
+    private HashMap<Student, Grade> cgrade;
+    public Course(){
+        cname = "Course01";
+        cid = "000";
+        semester = "Fall";
+        cyear = "2019";
+        ccriterion = new Criterion();
+        cgrade = new HashMap<>();
+    }
+    public Course(String name, String id, String s, String y, Criterion c){
+        cname = name;
+        cid = id;
+        semester = s;
+        cyear = y;
+        ccriterion = c;
+        cgrade = new HashMap<>();
+    }
+    public void enrollStudent(Student s){
+        Grade temp = new Grade(ccriterion);
+        cgrade.put(s,temp);
+    }
+    public HashMap<Student, Grade> getList(){
+        return cgrade;
+    }
+    public String[] getInfo(){
+        String[] res =new String[]{cname,cid,semester,cyear};
+        return res;
+    }
+    public String[] getAnalysis(){
+        String[] res = new String[4];
+        return res;
+    }
+    public double normalizeGrade(Grade g){
+        double total = 0;
+        for(int i=0;i<ccriterion.getNumberOfAssignments();i++){
+            String oc = g.getAssignment(i).getScore();
+            CriComp temp = ccriterion.getAssignments().get(i);
+            double fs = temp.getToatalScore();
+            double sc;
+            if(oc.charAt(oc.length()-1)=='%'){
+                sc = Double.valueOf(oc.substring(0,oc.length()-2))/100;
+            } else if(oc.charAt(0)=='-'){
+                sc = (fs-Double.valueOf(oc.substring(1,oc.length()-1)))/fs;
+            } else {
+                sc = Double.valueOf(oc)/fs;
+            }
+            total+=sc*temp.getWeights()*100;
+        }
+        for(int i=0;i<ccriterion.getNumberOfExams();i++){
+            String oc = g.getExam(i).getScore();
+            CriComp temp = ccriterion.getExams().get(i);
+            double fs = temp.getToatalScore();
+            double sc;
+            if(oc.charAt(oc.length()-1)=='%'){
+                sc = Double.valueOf(oc.substring(0,oc.length()-2))/100;
+            } else if(oc.charAt(0)=='-'){
+                sc = (fs-Double.valueOf(oc.substring(1,oc.length()-1)))/fs;
+            } else {
+                sc = Double.valueOf(oc)/fs;
+            }
+            total+=sc*temp.getWeights()*100;
+        }
+        for(int i=0;i<ccriterion.getNumberOfProjects();i++){
+            String oc = g.getProject(i).getScore();
+            CriComp temp = ccriterion.getProjects().get(i);
+            double fs = temp.getToatalScore();
+            double sc;
+            if(oc.charAt(oc.length()-1)=='%'){
+                sc = Double.valueOf(oc.substring(0,oc.length()-2))/100;
+            } else if(oc.charAt(0)=='-'){
+                sc = (fs-Double.valueOf(oc.substring(1,oc.length()-1)))/fs;
+            } else {
+                sc = Double.valueOf(oc)/fs;
+            }
+            total+=sc*temp.getWeights()*100;
+        }
+        String att = g.getAttendence().getScore();
+        double attsc;
+        if(att.charAt(att.length()-1)=='%'){
+            attsc = Double.valueOf(att.substring(0,att.length()-2))/100;
+        }else if(att.charAt(0)=='-'){
+            attsc = (100-Double.valueOf(att.substring(1,att.length()-1)))/100;
+        }else{
+            attsc = Double.valueOf(att)/100;
+        }
+        total+=attsc*ccriterion.getWeightsOfAttendance()*100;
+        return total;
+    }
 }
