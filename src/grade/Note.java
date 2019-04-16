975825594bc8dfd651f9f5b7fdc64ee1a57153cf
mmao95 <zhumaomao509@gmail.com>
2019-04-16 16:10:29 -0400

    Update Note.java

--- src/grade/Note.java
+++ src/grade/Note.java
@@ -1,5 +1,17 @@
 package grade;
 
 public class Note {
-
+    private String content;
+    public Note() {
+        content = "";
+    }
+    public Note(String s) {
+        content = s;
+    }
+    public void setNote(String s) {
+        content = s;
+    }
+    public String getNote() {
+        return content;
+    }
 }
