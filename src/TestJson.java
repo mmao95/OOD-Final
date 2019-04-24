e8c671299e7e9d3144c34be2fbd32468fe4ba0dc
Zhizhou Qiu <qzhizhou@bu.edu>
2019-04-24 15:46:17 -0400

    json

--- src/TestJson.java
+++ src/TestJson.java
@@ -1,4 +1,9 @@
+import course.Course;
+import course.Criterion;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,10 +19,41 @@ public class TestJson {
         JSONParser jsonParser = new JSONParser();
 
         try{
-            FileReader fileReader = new FileReader("");
+            FileReader fileReader = new FileReader("raw.json");
 
-        } catch (IOException io){
-            io.printStackTrace();
+            Object obj = jsonParser.parse(fileReader);
+
+            JSONArray jsonArray = (JSONArray) obj;
+
+            JSONObject jsoncourse = (JSONObject) jsonArray.get(0);
+
+            JSONObject jsonCriterion = (JSONObject) jsonArray.get(1);
+
+            Criterion criterion = new Criterion(
+                    Double.valueOf(jsonCriterion.get("weightsOfAssignments").toString()),
+                    Double.valueOf(jsonCriterion.get("weightsOfExams").toString()),
+                    Double.valueOf(jsonCriterion.get("weightsOfProjects").toString()),
+                    Double.valueOf(jsonCriterion.get("weightsOfAttendance").toString()),
+                    Integer.valueOf(jsonCriterion.get("numberOfAssignments").toString()),
+                    Integer.valueOf(jsonCriterion.get("numberOfExams").toString()),
+                    Integer.valueOf(jsonCriterion.get("numberOfProjects").toString()),
+                    jsonCriterion.get("name").toString()
+            );
+
+            Course course = new Course(
+                    jsoncourse.get("name").toString(),
+                    jsoncourse.get("id").toString(),
+                    jsoncourse.get("semester").toString(),
+                    jsoncourse.get("year").toString(),
+                    criterion
+            );
+
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (ParseException e){
+            e.printStackTrace();
+        } catch (IndexOutOfBoundsException e){
+            e.printStackTrace();
         }
 
 
