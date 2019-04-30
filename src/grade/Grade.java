00c955b483d62f65e0f17278b19a64f54894ba15
mmao95 <zhumaomao509@gmail.com>
2019-04-30 18:19:30 -0400

    Update Grade.java

--- src/grade/Grade.java
+++ src/grade/Grade.java
@@ -2,6 +2,8 @@ package grade;
 
 
 import course.Criterion;
+import course.NewCriterion;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,87 +14,40 @@ import java.util.List;
  **/
 public class Grade implements Serializable,Comparable{
     private String uid;
-    private List<GradeComp> aGrade;
-    private List<GradeComp> eGrade;
-    private List<GradeComp> pGrade;
+    private List<List<GradeComp>> grade;
     private GradeComp attendence;
     private GradeComp extraCredit;
     private double ttscore = 0;
-    private double att = 0;
-    private double ett = 0;
-    private double ptt = 0;
     //default constructor
     public Grade(){
-        aGrade = new ArrayList<GradeComp>();
-        eGrade = new ArrayList<GradeComp>();
-        pGrade = new ArrayList<GradeComp>();
+        grade = new ArrayList<>();
         attendence = new GradeComp();
+        extraCredit = new GradeComp();
     }
-    public Grade(Criterion c,String id){
+    public Grade(NewCriterion c, String id){
         uid = id;
-        aGrade = new ArrayList<GradeComp>();
-        eGrade = new ArrayList<GradeComp>();
-        pGrade = new ArrayList<GradeComp>();
+        grade = new ArrayList<>();
         attendence = new GradeComp();
-        GradeComp temp = new GradeComp();
-        for(int i=0;i<c.getNumberOfAssignments();i++){
-            aGrade.add(temp);
-        }
-        for(int i=0;i<c.getNumberOfExams();i++){
-            eGrade.add(temp);
-        }
-        for(int i=0;i<c.getNumberOfProjects();i++){
-            pGrade.add(temp);
+        extraCredit = new GradeComp();
+        for(int i=0;i<c.getCategories().size();i++){
+            List<GradeComp> temp = new ArrayList<>();
+            for(int j=0;j<c.getCategories().get(i).getCriComps().size();j++){
+                temp.add(new GradeComp());
+            }
+            grade.add(temp);
         }
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("grade of assignments: ");
-        for (GradeComp gradeComp : aGrade){
-            sb.append(gradeComp.getScore());
-            sb.append(" ");
-        }
-        sb.append("\n");
-        sb.append("grade of exams: ");
-        for (GradeComp gradeComp :  eGrade){
-            sb.append(gradeComp.getScore());
-            sb.append(" ");
-        }
-        sb.append("\n");
-        sb.append("grade of projects: ");
-        for (GradeComp gradeComp : pGrade){
-            sb.append(gradeComp.getScore());
-            sb.append(" ");
-        }
-        sb.append("\n");
-        sb.append("grade of attendance: ");
-        sb.append(attendence.getScore());
-        return sb.toString();
-    }
-
-    //get list of grade of assignment/exam/project
-    public List<GradeComp> getaGrade(){
-        return aGrade;
-    }
-    public List<GradeComp> geteGrade(){
-        return eGrade;
-    }
-    public List<GradeComp> getpGrade() {
-        return pGrade;
+    //get list of grade of one category
+    public List<GradeComp> getCategory(int cat){
+        return grade.get(cat);
     }
     //get particular assignment/exam/project
+    public GradeComp getOne(int cat, int index){
+        return grade.get(cat).get(index);
+    }
     public String getUid(){ return uid; }
-    public GradeComp getAssignment(int ind){
-        return aGrade.get(ind);
-    }
-    public GradeComp getExam(int ind){
-        return eGrade.get(ind);
-    }
-    public GradeComp getProject(int ind){
-        return pGrade.get(ind);
-    }
+
     public GradeComp getAttendence(){
         return attendence;
     }
@@ -100,30 +55,12 @@ public class Grade implements Serializable,Comparable{
         return extraCredit;
     }
     //add new assignment/exam/project
-    public void addAssignment(){
-        GradeComp temp = new GradeComp("0");
-        aGrade.add(temp);
-    }
-    public void addExam(){
-        GradeComp temp = new GradeComp("0");
-        eGrade.add(temp);
-    }
-    public void addProject(){
-        GradeComp temp = new GradeComp("0");
-        pGrade.add(temp);
+    public void addNew(int cat){
+        grade.get(cat).add(new GradeComp());
     }
     //setter
-    public void setAssignment(String s, int ind){
-        GradeComp temp = new GradeComp(s);
-        aGrade.set(ind,temp);
-    }
-    public void setExam(String s, int ind){
-        GradeComp temp = new GradeComp(s);
-        eGrade.set(ind,temp);
-    }
-    public void setProject(String s, int ind){
-        GradeComp temp = new GradeComp(s);
-        pGrade.set(ind,temp);
+    public void setUid(String s){
+        uid = s;
     }
     public void setTtscore(double a){
         ttscore = a;
@@ -135,16 +72,6 @@ public class Grade implements Serializable,Comparable{
         Double b = ((Grade) o).getTtscore();
         return a.compareTo(b);
     }
-
-    public void setAtt(double a){
-        att = a;
-    }
-    public void setEtt(double a){
-        ett = a;
-    }
-    public void setptt(double a){
-        ptt = a;
-    }
     public void setAttendence(String s){
         attendence = new GradeComp(s);
     }
@@ -154,14 +81,5 @@ public class Grade implements Serializable,Comparable{
     //getter
     public double getTtscore(){
         return ttscore;
-    }
-    public double getAtt(){
-        return att;
-    }
-    public double getEtt(){
-        return ett;
-    }
-    public double getPtt(){
-        return ptt;
     }
 }
