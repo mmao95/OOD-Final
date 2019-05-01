3274a25b391ba8ebe56379ad0c47e13e7b19723d
wqtwjt1996 <1061687050@qq.com>
2019-04-30 22:57:24 -0400

    Update SubCategory.java

--- src/frame/SubCategory.java
+++ src/frame/SubCategory.java
@@ -1,6 +1,7 @@
-package frame;
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
 
 import javax.swing.*;
 import javax.swing.table.TableColumn;
@@ -11,22 +12,26 @@ import javax.swing.table.DefaultTableCellRenderer;
  * @Date: 04-12-2019 20:57
  * @Description:
  */
-public class SubCategory extends JFrame{
+public class SubCategory extends JFrame implements ActionListener {
     protected JButton okButton;
     protected JLabel weightString;
     protected JTable table;
     protected JScrollPane tableScrollPane;
     protected JPanel weightPanel, tablePanel, buttonPanel;
     protected TableColumn column;
+    private int itemNumber = 0;
 
-    public SubCategory(){
+    public SubCategory(int num){
+        this.itemNumber = num;
+
         okButton = new JButton("OK");
-        String[] tableHead = {"Assignment", "Score", "Weight"};
+        okButton.addActionListener(this);
+        String[] tableHead = {"Assignment", "Weight"};
         Object[][] data = {
-                {"Assignment", "Student Score", "Weight"},
-                {"Assignment 1", new Double(91.3), new Double(0.6)},
-                {"Assignment 2", new Double(97.5), new Double(0.2)},
-                {"Assignment 3", new Double(94.1), new Double(0.2)}
+                {"Assignment", "Weight"},
+                {"Assignment 1", new Double(0.6)},
+                {"Assignment 2", new Double(0.2)},
+                {"Assignment 3", new Double(0.2)}
         };
         table = new JTable(data, tableHead);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
@@ -35,7 +40,7 @@ public class SubCategory extends JFrame{
         table.setGridColor(Color.gray);
         table.setRowHeight(50);// Setting the width of rol
         table.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);// Setting the width of column
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             column = table.getColumnModel().getColumn(i);
             column.setPreferredWidth(200);
         }
@@ -56,9 +61,9 @@ public class SubCategory extends JFrame{
         tablePanel.add(table);
         buttonPanel.add(okButton);
 
-        weightPanel.setBounds(0, 20, 600, 50);
-        tablePanel.setBounds(0, 50, 600, 250);
-        buttonPanel.setBounds(250, 270, 100, 350);
+        weightPanel.setBounds(0, 20, 400, 50);
+        tablePanel.setBounds(0, 50, 400, 250);
+        buttonPanel.setBounds(150, 270, 100, 350);
 
         this.add(buttonPanel);
         this.add(tablePanel);
@@ -66,11 +71,58 @@ public class SubCategory extends JFrame{
 
         this.setTitle("Sub-category");
 //        this.setLayout(new GridLayout(3, 1));
-        this.setSize(600, 350);
+        this.setSize(400, 350);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         this.setVisible(true);
         this.setResizable(false);
+    }
+
+    private boolean isDouble(String str) {
+        if (null == str || "".equals(str)) {
+            return false;
         }
+        Pattern pattern = Pattern.compile("^[-\\+]?[.\\d]*$");
+        if(pattern.matcher(str).matches()){
+            if(Double.parseDouble(str) >= 0.0 && Double.parseDouble(str) <= 1.0)
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+    private boolean judgeWeightNumber(){
+        boolean returnValue = true;
+        for (int i = 1; i < itemNumber; i++){
+            if(!isDouble(table.getValueAt(i, 1).toString()))
+                returnValue = false;
+        }
+        return returnValue;
+    }
+
+    private boolean judgeWeightSum(){
+        double sum = 0.0;
+        for (int i = 1; i < itemNumber; i++)
+            sum += Double.parseDouble(table.getValueAt(i, 1).toString());
+        if(sum == 1.0)
+            return true;
+        else
+            return false;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand()=="OK") {
+            if(judgeWeightNumber()) {
+                if(judgeWeightSum())
+                    System.out.println("233");
+                else
+                    JOptionPane.showMessageDialog(null, "The sum of item weight must be 1.0!", "Info", JOptionPane.WARNING_MESSAGE);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Item weight must be float ranging from 0 to 1!", "Info", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }
 }
